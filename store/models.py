922dc56e691d01e85d4b06abdf5ff5ba28849bf5
BrandonLCC <br.casas@duocuc.cl>
2024-11-09 20:53:08 -0300

    ignorando entorno virutal .gititgnore

--- store/models.py
+++ store/models.py
@@ -75,4 +75,6 @@ class ElementoCarrito(models.Model):
     carrito = models.ForeignKey(Carrito, on_delete=models.CASCADE)
     producto = models.ForeignKey(Producto, on_delete=models.CASCADE)
     cantidad = models.PositiveIntegerField(default=1)
-
+#
+class Ventas(models.Model):
+    id_venta = models.CharField(primary_key = True)
